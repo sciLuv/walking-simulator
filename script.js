810df76e08e8d9dd129b6bf122e2db82868603b5
sciluv <sci.luv.inc@gmail.com>
2021-06-03 03:37:54 +0200

    js more lisible, add distinction html scroll and in game scroll, scroll working in y bottom

--- script.js
+++ script.js
@@ -2,59 +2,74 @@ let scrollLand = document.getElementById('render-3d');
 
 //base de la transformation du render-3d et des chunk 
 let base3dScroll = "rotateX(45deg) rotateY(0deg) rotateZ(10deg) skewX(-25deg) skewY(10deg)";
-//valeur initial du scrolling
-let scrollPositionX = 0;
-let scrollPositionY = 0;
-
-let generateChunk = 4; /*clef identité chunk*/
-let gateChunk = 4 /*vérifie si il n'y a pas déja suffissament de chunk*/
-let nextBotRightGen = -160; /*pour l'instant ajoute un chuck quand on ArrowDown il sera necessaire d'en faire 4 version */
-let removedChunck = 0;
+//valeur initial du scrolling Ingame et HTML
+let position = {
+    inGame : {x: 0, y: 0},
+    html : {x: 0, y: 0}
+}
 
 // base du code HTML des chunk, sans l'entete qu'on rajoute ensuite dans changementchunk, pour y mettre leur id.
 let chunkHTML = "";
 for(i=0; i<=48; i++){
-    console.log(i);
     chunkHTML += '<div class="content"></div>';
     if(i == 48){
        chunkHTML += '</div>';
     }
 };
 
+let chunkID = 4; /*genere clef identité chunk*/
+let XGenBotton = -158; /*pour l'instant ajoute un chuck quand on ArrowDown il sera necessaire d'en faire 4 version */
+let XGenTop = 110;
+let XTopRemove = -495;
+let topXRemoveNum = 1;
+let removedChunck = 0;
+
 //commande, a retravaillé vis a vis des saccades
 body.addEventListener('keydown', move);
 function move(event){
     let key = event.code;
     if (key == "ArrowDown"){
-        scrollPositionY -= 10;
+        position.html.y -= 10;
+        position.inGame.y -= 10;
     }
     if (key == "ArrowUp"){
-        scrollPositionY += 10;
+        position.html.y += 10;
+        position.inGame.y += 10;
     }
     if (key == "ArrowLeft"){
-        scrollPositionX += 10;
+        position.html.x += 10;
+        position.inGame.x += 10;
     }
     if (key == "ArrowRight") {
-        scrollPositionX -= 10;
+        position.html.x -= 10;
+        position.inGame.x -= 10;
     }
-    scrollLand.style.transform = base3dScroll + "translate("+ scrollPositionX + "px, " + scrollPositionY + "px)"; 
+    scrollLand.style.transform = base3dScroll + "translate("+ position.html.x + "px, " + position.html.y + "px)"; 
+    /*console.log("Y : HTML " + position.html.y + ", INGAME " + position.inGame.y);
+    console.log("X : HTML " + position.html.x + ", INGAME" + position.inGame.x);*/
 }
 
 function changementChunk(){
-    //pour la génération en bas a droite
-    if ((scrollPositionY <= nextBotRightGen)&&(generateChunk==gateChunk)){
+    //generation xbottom
+    if ((position.inGame.y <= XGenBotton)){
         for(i=0; i<=1; i++){
-            generateChunk ++;
-            gateChunk ++;
-            scrollLand.innerHTML += '<div id="chunk-' + generateChunk + '" class="chunk">' + chunkHTML;
+            chunkID ++;
+            scrollLand.innerHTML += '<div id="chunk-' + chunkID + '" class="chunk">' + chunkHTML;
         }
-       nextBotRightGen = nextBotRightGen + (-630); 
+       XGenBotton = XGenBotton + (-630); 
     }
-    if ((scrollPositionY <= -495)&&(removedChunck <= 2)){
-        for(i=0; i<=1; i++){
-            let forgotedChunk = document.getElementById("chunk-"+(i+1));
+    //suppression xtop
+    if (position.html.y <= XTopRemove){
+        for(i=0; i<=1; i++){ 
+            //partie a retravaillé pour que la gen fonctionne avec les autre sens (placé les div on bon endroit grace a array)
+            let forgotedChunk = document.getElementById("chunk-"+(topXRemoveNum));
+            topXRemoveNum ++;
+            forgotedChunk.remove();
         }
+        position.html.y = 112;
+        scrollLand.style.transform = base3dScroll + "translate("+ position.html.x + "px, " + position.html.y + "px)"; 
     }
+    
 }
 
-let tcheckChunk = setInterval(changementChunk, 20);
+let tcheckChunk = setInterval(changementChunk, 200);
